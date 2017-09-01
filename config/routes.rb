2d7c66c8732be4bb1f992ec857d0b0026090d8cf
Treven Trujillo <treventrujillo@gmail.com>
2017-09-01 17:12:53 -0600

    functioning user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'shops#index'
+
   get 'items/index'
 
   get 'items/show'
@@ -10,5 +12,4 @@ Rails.application.routes.draw do
   get 'carts/show'
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
